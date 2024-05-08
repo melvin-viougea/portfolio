c6eb186779db32b8078804f3bd54f9935a132b59
Melvin Viougea <mviougea@icloud.com>
2024-05-08 22:34:21 +0200

    update analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Anek_Telugu } from "next/font/google";
 import "./globals.css";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
+import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import {cn} from "@/lib/utils";
 
@@ -32,6 +33,7 @@ export default function RootLayout({
         )}
       >
         {children}
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
